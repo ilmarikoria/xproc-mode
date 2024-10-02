2d8af709a883438c1d15ab5f8d5fa07e30d36aa2
Ilmari Koria <ilmari.koria@bloomsbury.com>
2024-10-02 10:38:25 +0100

    init

---
+++ xproc.el
@@ -0,0 +1,30 @@
+;;;; xproc.el --- GNU Emacs minor mode for creating XProc pipelines
+;; Copyright (C) 2024 Ilmari Koria
+
+;; Author: Ilmari Koria
+;; Created: 02 Oct 2024
+;; Keywords: xml, pipelines, xsl, publishing
+
+;;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 2
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun print-hello-world ()
+  (interactive)
+  (message "你好世界"))
+
+;;; xproc.el ends here
